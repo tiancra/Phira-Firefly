ff6a4b76557cc4afa3b81126b51022710f141fa1
tiancra <tcwbi@outlook.com>
2026-06-19 02:02:21 +0800

    Remove Android restriction for BGM loading

--- phira/src/scene/main.rs
+++ phira/src/scene/main.rs
@@ -92,9 +92,6 @@ impl MainScene {
     pub async fn new(fallback: FontArc) -> Result<Self> {
         Self::init().await?;
 
-        #[cfg(target_os = "android")]
-        let bgm = None;
-        #[cfg(not(target_os = "android"))]
         let bgm = {
             match AudioClip::new(load_file("bgm.mp3").await?) {
                 Ok(clip) => Some(UI_AUDIO.with(|it| {
